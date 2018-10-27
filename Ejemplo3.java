e489971d365f9c37294d602e2b58f1dbdbc220bd
Oscar-182T0169 <44146540+Oscar-182T0169@users.noreply.github.com>
2018-10-27 00:18:13 -0500

    Add files via upload

---
+++ Ejemplo3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ejemplo3{
+public static void main(String args[]){
+Scanner leer = new Scanner(System.in);
+String nombre;
+System.out.println("Ingresa tu nombre: ");
+nombre = leer.nextLine();
+System.out.println("Hola "+nombre);
+   }
+} 
