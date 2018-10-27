b645c1ec4b9069489a384adf85a7ce305270b641
Oscar-182T0169 <44146540+Oscar-182T0169@users.noreply.github.com>
2018-10-27 00:17:59 -0500

    Add files via upload

---
+++ ejemplo2.java
@@ -0,0 +1,14 @@
+public class ejemplo2{
+public static void main(String args[]){
+
+   int valor1  =2345;
+   char x= 1,z= 3;
+   String nombre= "Juan Antonio Sanches";
+   double xy = 2.33345;
+   float y= 2.3f;
+   System.out.println("el valor1 =" +valor1);
+   System.out.println("el valor de x: "+x+" valor de z: "+z);
+   System.out.println("su nombre es: "+nombre);
+   System.out.println("su valor xy: "+xy);
+  }
+}   
